a523f697925b3fb7fc91d58c41f11b3edeae64e5
paolo-della-fera <paolodf2005@gmail.com>
2026-03-12 15:41:53 +0100

    complete milestone 1: add item list markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
+import AppHeader from "./components/AppHeader"
+
+
+
+const articles = [
+  { id: 1, title: "Verstappen domina il GP di Monaco" },
+  { id: 2, title: "Ferrari: nuovi aggiornamenti in arrivo" },
+  { id: 3, title: "Hamilton firma con la Ferrari" },
+  { id: 4, title: "Leclerc in pole a Silverstone" },
+  { id: 5, title: "La McLaren sorprende tutti a Monza" },
+  { id: 6, title: "Norris: la mia stagione migliore" },
+  { id: 7, title: "GP del Giappone: trionfa Verstappen" },
+  { id: 8, title: "Mercedes torna competitiva?" },
+];
+
 
 
 function App() {
-  
+
 
   return (
     <>
-      
+      <AppHeader />
+
+      <div className="container text-center mt-4">
+        <ul className="list-group">
+          {
+            articles.map((article) => (
+              <li className="list-group-item" key={article.id}>{article.title}</li>
+            ))
+          }
+        </ul>
+      </div>
+
     </>
   )
 }
