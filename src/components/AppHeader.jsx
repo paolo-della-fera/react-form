9f99353fa55764bcff77295195e2e28e91ddba21
paolo-della-fera <paolodf2005@gmail.com>
2026-03-12 17:56:11 +0100

    add background image to main section and card style

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,7 +4,7 @@ export default function AppHeader() {
   return (
     <header>
       <div className="container">
-        <h1 className="text-center fw-bold">Article List</h1>
+        <h1 className="text-center fw-bold">F1 Article List</h1>
       </div>
 
     </header>
