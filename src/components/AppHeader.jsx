4343b6ad49997fc978caa635bf6e23271293cdd4
paolo-della-fera <paolodf2005@gmail.com>
2026-03-12 15:41:05 +0100

    add page header with it's style

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,12 @@
+export default function AppHeader() {
+
+
+  return (
+    <header>
+      <div className="container">
+        <h1 className="text-center fw-bold">Article List</h1>
+      </div>
+
+    </header>
+  )
+}
